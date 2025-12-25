ed16c911ec66607b4704af3832273f6f9f723c49
VladSemyonov <139633734+astraaskarida@users.noreply.github.com>
2025-12-25 18:59:46 +0300

    Add files via upload

---
+++ Laba6(1).py
@@ -0,0 +1,21 @@
+class UserAccount:
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.__password = password
+
+    def set_password(self, new_password):
+        self.__password = new_password
+
+    def check_password(self, password):
+        return password == self.__password
+
+
+user = UserAccount("abcd", "abcd@mail.ru", "123456")
+
+print(user.check_password("123456"))
+print(user.check_password("000000"))
+
+user.set_password("newpass")
+print(user.check_password("newpass"))
